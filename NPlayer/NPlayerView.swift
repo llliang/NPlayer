1eb50067772dc1af1ff8f750c734bc3ac56f5a72
Jiang Liang <bluedream5161@gmail.com>
2019-12-09 10:59:32 +0800

    播放器

---
+++ NPlayer/NPlayerView.swift
@@ -0,0 +1,26 @@
+//
+//  NPlayerView.swift
+//  NPlayer
+//
+//  Created by Neo on 2019/12/9.
+//  Copyright © 2019 TaiHao. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+public class NPlayerView: UIView {
+    override public class var layerClass: AnyClass {
+        get {
+            return AVPlayerLayer.self
+        }
+    }
+    
+    public var playerLayer: AVPlayerLayer {
+        get {
+            let avLayer = self.layer as! AVPlayerLayer
+            avLayer.videoGravity = .resizeAspectFill
+            return avLayer
+        }
+    }
+}
