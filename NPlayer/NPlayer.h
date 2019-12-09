1bdfb4edb2389c9667ff97de1641050a84400d0c
Jiang Liang <bluedream5161@gmail.com>
2019-12-09 10:46:33 +0800

    Initial Commit

---
+++ NPlayer/NPlayer.h
@@ -0,0 +1,19 @@
+//
+//  NPlayer.h
+//  NPlayer
+//
+//  Created by Neo on 2019/12/9.
+//  Copyright © 2019 TaiHao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NPlayer.
+FOUNDATION_EXPORT double NPlayerVersionNumber;
+
+//! Project version string for NPlayer.
+FOUNDATION_EXPORT const unsigned char NPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NPlayer/PublicHeader.h>
+
+
